b72bbadf6afd36b737c396a66f8039332ecd9e2f
Tim Moore <tcmoore3@gmail.com>
2016-08-30 17:09:18 -0500

    Read all coefficients from a single file

--- linear_solver/core.py
+++ linear_solver/core.py
@@ -7,18 +7,25 @@ def _check_inverse(coeffs):
     if np.isclose(det, 0.0):
         raise ZeroDivisionError
 
-def _load_files(coefficients_file, constants_file):
-    coeffs = np.matrix([[eval(x) for x in row] for row in 
-        np.genfromtxt(coefficients_file, dtype=str)])
-    consts = np.matrix([[eval(x) for x in row] for row in 
-        np.genfromtxt(constants_file, dtype=str).reshape((-1, 1))])
-    return coeffs, consts
+def _load_coefficients(coefficients_file):
+    coeffs = np.genfromtxt(coefficients_file, dtype=str)
+    assert(coeffs.ndim == 2)#, 'Input matrix must be 2 dimensional')
+    assert(coeffs.shape[0]+1 == coeffs.shape[1])
+    coeffs = np.matrix([[eval(x) for x in row] for row in coeffs])
+    return coeffs
 
-def solve_linear_system(coefficients_file, constants_file):
-    coeffs, consts = _load_files(coefficients_file, constants_file)
+def _check_matrices(coeffs, consts):
+    assert(coeffs.shape[0] == coeffs.shape[1])
+    assert(coeffs.shape[0] == consts.shape[0])
+    assert(consts.shape[1] == 1)
+
+def solve_linear_system(coefficients_file):
+    coeffs = _load_coefficients(coefficients_file)
+    a, b = coeffs[:, :-1], coeffs[:, -1]
+    _check_matrices(a, b)
     try:
-        _check_inverse(coeffs)
-        return coeffs.I * consts
+        _check_inverse(a)
+        return a.I * b
     except ZeroDivisionError:
-        print('Determinant of coefficients matrix is 0. No solution.')
+        print('Determinant of coefficients matrix is 0. No unique solution.')
         return None
--- linear_solver/tests/tests.py
+++ linear_solver/tests/tests.py
@@ -4,14 +4,14 @@ from linear_solver.core import solve_linear_system
 from linear_solver.utils.general import get_fn
 
 def test_zero_determinant():
-    x = solve_linear_system(get_fn('p1coeffs.txt'), get_fn('p1consts.txt'))
+    x = solve_linear_system(get_fn('p1coeffs.txt'))
     assert(x == None)
 
 def test_integer_coefficients():
-    x = solve_linear_system(get_fn('p2coeffs.txt'), get_fn('p2consts.txt'))
+    x = solve_linear_system(get_fn('p2coeffs.txt'))
     assert(x.shape == (2, 1))
 
 def test_eval_expressions():
-    x = solve_linear_system(get_fn('p2coeffs.txt'), get_fn('p2consts.txt'))
-    y = solve_linear_system(get_fn('exp_coeffs.txt'), get_fn('exp_consts.txt'))
+    x = solve_linear_system(get_fn('p2coeffs.txt'))
+    y = solve_linear_system(get_fn('exp_coeffs.txt'))
     assert(np.all(x==y))
