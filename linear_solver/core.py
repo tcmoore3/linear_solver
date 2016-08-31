ed0fe86c8998b85769524fbf522d14d2d0fdaa5f
Tim Moore <tcmoore3@gmail.com>
2016-08-31 08:21:46 -0500

    Add ability to pass in lists and arrays to solver
    You can now either load the coefficients from a text file, or just pass
    them straight into the function call.
    I see this being useful for interactive sessions.
    Also add a bunch of unit tests for this feature.

--- linear_solver/core.py
+++ linear_solver/core.py
@@ -7,10 +7,13 @@ def _check_inverse(coeffs):
     if np.isclose(det, 0.0):
         raise ZeroDivisionError
 
-def _load_coefficients(coefficients_file):
-    coeffs = np.genfromtxt(coefficients_file, dtype=str)
+def _matrix_sanity(coeffs):
     assert(coeffs.ndim == 2)#, 'Input matrix must be 2 dimensional')
     assert(coeffs.shape[0]+1 == coeffs.shape[1])
+
+def _load_coefficients(coefficients_file):
+    coeffs = np.genfromtxt(coefficients_file, dtype=str)
+    _matrix_sanity(coeffs)
     coeffs = np.matrix([[eval(x) for x in row] for row in coeffs])
     return coeffs
 
@@ -19,13 +22,35 @@ def _check_matrices(coeffs, consts):
     assert(coeffs.shape[0] == consts.shape[0])
     assert(consts.shape[1] == 1)
 
-def solve_linear_system(coefficients_file):
-    coeffs = _load_coefficients(coefficients_file)
-    a, b = coeffs[:, :-1], coeffs[:, -1]
-    _check_matrices(a, b)
+def _list_to_matrix(coefficients):
+    for l in coefficients:
+        assert(len(l) == len(coefficients[0]))
+    coeffs = np.matrix(coefficients)
+    _matrix_sanity(coeffs)
+    return coeffs
+
+def handle_input(coefficients):
+    if isinstance(coefficients, str):
+        return _load_coefficients(coefficients)
+    elif isinstance(coefficients, list):
+        return _list_to_matrix(coefficients)
+    elif isinstance(coefficients, np.ndarray):
+        _matrix_sanity(coefficients)
+        return np.matrix(coefficients)
+    elif isinstance(coefficients, np.matrix):
+        _matrix_sanity(coefficients)
+        return coefficients
+    else:
+        raise(TypeError, 'Unsupported input type.')
+
+def solve_linear_system(coefficients):
+    coeffs = handle_input(coefficients)
+    A = coeffs[:, :-1]
+    B = coeffs[:, -1].reshape((-1, 1))
+    _check_matrices(A, B)
     try:
-        _check_inverse(a)
-        return a.I * b
+        _check_inverse(A)
+        return A.I * B
     except ZeroDivisionError:
         print('Determinant of coefficients matrix is 0. No unique solution.')
         return None
--- linear_solver/tests/tests.py
+++ linear_solver/tests/tests.py
@@ -1,17 +1,63 @@
 import numpy as np
+import unittest
 
 from linear_solver.core import solve_linear_system
 from linear_solver.utils.general import get_fn
 
-def test_zero_determinant():
-    x = solve_linear_system(get_fn('p1coeffs.txt'))
-    assert(x == None)
+class TestSolverMethods(unittest.TestCase):
+    def test_zero_determinant(self):
+        x = solve_linear_system(get_fn('p1coeffs.txt'))
+        self.assertEqual(x, None)
 
-def test_integer_coefficients():
-    x = solve_linear_system(get_fn('p2coeffs.txt'))
-    assert(x.shape == (2, 1))
+    def test_integer_coefficients(self):
+        x = solve_linear_system(get_fn('p2coeffs.txt'))
+        self.assertEqual(x.shape, (2, 1))
 
-def test_eval_expressions():
-    x = solve_linear_system(get_fn('p2coeffs.txt'))
-    y = solve_linear_system(get_fn('exp_coeffs.txt'))
-    assert(np.all(x==y))
+    def test_eval_expressions(self):
+        x = solve_linear_system(get_fn('p2coeffs.txt'))
+        y = solve_linear_system(get_fn('exp_coeffs.txt'))
+        self.assertTrue(np.all(x==y))
+
+    def test_good_list_input(self):
+        x = solve_linear_system([
+            [22, 22, -1, 0],
+            [0.1*22, 22*0.9, -0.6, 0],
+            [22/0.68, 22/0.78, 0, 500*3.78541]])
+
+    def test_bad_list_input(self):
+        with self.assertRaises(AssertionError):
+            x = solve_linear_system([
+                [22, 22, -1, 0],
+                [0.1*22, 22*0.9, -0.6, 0],
+                [22/0.68, 22/0.78, 500*3.78541]])
+
+    def test_good_array_input(self):
+        x = solve_linear_system(
+            np.asarray([
+            [22, 22, -1, 0],
+            [0.1*22, 22*0.9, -0.6, 0],
+            [22/0.68, 22/0.78, 0, 500*3.78541]]))
+
+    def test_bad_array_input(self):
+        x = solve_linear_system(
+            np.asarray([
+            [22, 22, -1, 0],
+            [0.1*22, 22*0.9, -0.6, 0],
+            [22/0.68, 22/0.78, 0, 500*3.78541]]))
+
+    def test_good_matrix_input(self):
+        x = solve_linear_system(
+            np.matrix([
+            [22, 22, -1, 0],
+            [0.1*22, 22*0.9, -0.6, 0],
+            [22/0.68, 22/0.78, 0, 500*3.78541]]))
+
+    def test_bad_matrix_input(self):
+        x = solve_linear_system(
+            np.matrix([
+            [22, 22, -1, 0],
+            [0.1*22, 22*0.9, -0.6, 0],
+            [22/0.68, 22/0.78, 0, 500*3.78541]]))
+
+if __name__ == '__main__':
+    unittest.main()
